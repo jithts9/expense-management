83a98668279360fce15000a30c01bac512619f0c
jithts <jith.soman@aspiresys.com>
2024-10-10 01:19:11 +0530

    connected mongodb

--- src/server.js
+++ src/server.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_CONNECT_STRING)
+  .then(() => {
+    console.log('connected to mongodb');
+  })
+  .catch((err) => {
+    console.log("not connected and got error", err);
+  });
 
 const app = express();
 
 app.listen(5000, () => {
-  console.log("server starting at 5000 port");
+  console.log("server starting at port 5000");
 });
