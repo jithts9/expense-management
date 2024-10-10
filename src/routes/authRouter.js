5288ad48473f218561794c2dbb1cc9e20bd2ce6c
jithts <jith.soman@aspiresys.com>
2024-10-10 17:23:59 +0530

    signin api

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,8 +1,9 @@
 import express from'express';
-import { signup } from '../controllers/auth.js';
+import { signup, signIn } from '../controllers/auth.js';
 
 const authRouter = express.Router()
 
 authRouter.post('/signup',signup )
+authRouter.post('/signin', signIn)
 
 export default authRouter;
